7a2cb962d379b1eb6d45c5129157782d6dc4fd56
jodhanikrunal <21ce045@charusat.edu.in>
2023-07-05 18:50:19 +0530

    minor change

--- Event Handaling & Task-2/signup.js
+++ Event Handaling & Task-2/signup.js
@@ -1,61 +1,74 @@
-const form = document.querySelector('form').value;
-const fname = document.querySelector('fname').value;
-const Uname = document.querySelector('Uname').value;
-const Password = document.querySelector('Password').value;
-const Confirm_Password = document.querySelector('Confirm_Password').value;
-const Ph_no = document.querySelector('Ph_no').value;
-const Email = document.querySelector('Email').value;
-const gender = document.querySelector('input[name="gender"]:checked');  
-const agreement = document.getElementById('agreement').checked;
+document.getElementById('signup').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-const isvalid = true;
+  const fname = document.getElementById('fname').value;
+  const Uname = document.getElementById('Uname').value;
+  const Password = document.getElementById('Password').value;
+  const Confirm_Password = document.getElementById('Confirm_Password').value;
+  const Ph_no = document.getElementById('Ph_no').value;
+  const Email = document.getElementById('Email').value;
+  const gender = document.querySelector('input[name="gender"]:checked');
+  const state = document.getElementById('State').value;
+  const agreement = document.getElementById('agreement').checked;
 
-if(!fname.matches(/^[a-zA-Z]+$/)){
-    document.getElementById('fnameErr').textContent = '* Enter valid name';
-    isvalid = false;
-}
+  let isValid = true;
 
-if(!Uname.matches(/^[a-zA-Z0-9]+$/)){
-    document.getElementById('UnameErr').textContent = '* Enter valid User name';
-    isvalid = false;
-}
+  document.getElementById('fnameErr').textContent = '';
+  document.getElementById('UnameErr').textContent = '';
+  document.getElementById('passwordErr').textContent = '';
+  document.getElementById('confirmpasswordErr').textContent = '';
+  document.getElementById('Ph_noErr').textContent = '';
+  document.getElementById('EmailErr').textContent = '';
+  document.getElementById('genderErr').textContent = '';
+  document.getElementById('StateErr').textContent = '';
+  document.getElementById('agreementError').textContent = '';
 
-if(!Password.matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,10}$/)){
+  if (!fname.match(/^[a-zA-Z]+$/)) {
+    document.getElementById('fnameErr').textContent = '* Enter a valid name';
+    isValid = false;
+  }
+
+  if (!Uname.match(/^[a-zA-Z0-9]+$/)) {
+    document.getElementById('UnameErr').textContent = '* Enter a valid username';
+    isValid = false;
+  }
+
+  if (!Password.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,10}$/)) {
     document.getElementById('passwordErr').textContent = '* Password must be alphanumeric with at least one special symbol (@$!%.*#?&) and have a length of 6 to 10 characters';
-    isvalid = false;
-}
+    isValid = false;
+  }
 
-if(Confirm_Password =! Password){
-    document.getElementById('confirmpasswordErr').textContent = '* Confirm Password must be match with password';
-    isvalid = false;
-}
+  if (Confirm_Password !== Password) {
+    document.getElementById('confirmpasswordErr').textContent = '* Confirm Password must match the password';
+    isValid = false;
+  }
 
-if(!Ph_no.match(/^\d{10}$/)){
-    document.getElementById('Ph_noErr').textContent = '* Enter valid phone no';
-    isvalid = false;
-}
+  if (!Ph_no.match(/^\d{10}$/)) {
+    document.getElementById('Ph_noErr').textContent = '* Enter a valid phone number';
+    isValid = false;
+  }
 
-if(!gender){
-    document.getElementById('genderError').textContent = '* Enter valid name';
-    isvalid = false;
-}
+  if (!gender) {
+    document.getElementById('genderErr').textContent = '* Select a gender';
+    isValid = false;
+  }
 
-if (!Email.match(/^[^\s@]+@charusat\.edu\.in$/  || /^[^\s@]+@charusat\.ac\.in$/)) {
-    document.getElementById('EmailError').textContent = '* Please enter a valid email address.';
-    isvalid = false;
+  if (!Email.match(/^[^\s@]+@charusat\.edu\.in$/)) {
+    document.getElementById('EmailErr').textContent = '* Please enter a valid email address ending with @charusat.edu.in';
+    isValid = false;
   }
 
   if (state === '') {
-    document.getElementById('StateError').textContent = '* Please select a state.';
-    isvalid = false;
+    document.getElementById('StateErr').textContent = '* Select a state';
+    isValid = false;
   }
 
   if (!agreement) {
-    document.getElementById('agreementError').textContent = '* You must agree to the terms and conditions.';
-    isvalid = false;
+    document.getElementById('agreementError').textContent = '* You must agree to the terms and conditions';
+    isValid = false;
   }
 
-  if (isvalid) {
+  if (isValid) {
     window.location.href = 'login.html';
   }
-
+});
